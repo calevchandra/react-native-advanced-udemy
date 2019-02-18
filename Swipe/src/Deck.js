65b185c2f851a1c88de6b17ae448a6b4269b5cd9
Calev Chandra <cbkavit@gmail.com>
2019-02-18 13:14:00 +1300

    25. Limiting Rotation with Interpolation

--- Swipe/src/Deck.js
+++ Swipe/src/Deck.js
@@ -1,9 +1,55 @@
 import React, { Component } from "react";
-import { View, Animated, Text } from "react-native";
+import { View, Animated, PanResponder, Dimensions } from "react-native";
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+
+    const position = new Animated.ValueXY();
+    const panResponder = PanResponder.create({
+      //on press
+      onStartShouldSetPanResponder: () => true,
+      //when element is dragged
+      onPanResponderMove: (event, gesture) => {
+        //debugger;
+        position.setValue({ x: gesture.dx, y: gesture.dy });
+      },
+      //after touch is released - let go
+      onPanResponderRelease: () => {}
+    });
+
+    this.state = { panResponder, position };
+  }
+
+  getCardStyle() {
+    const { position } = this.state;
+    //interpolation
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ["-120deg", "0deg", "120deg"]
+    });
+
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }]
+    };
+  }
+
   renderCards() {
-    return this.props.data.map(item => {
+    return this.props.data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            key={item.id}
+            style={this.getCardStyle()}
+            {...this.state.panResponder.panHandlers}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      }
       return this.props.renderCard(item);
     });
   }
