48a22ed075d45bfc07c5c422eb2b5c1da2742dd1
Calev Chandra <cbkavit@gmail.com>
2019-02-24 13:07:46 +1300

    30. Callbacks on Swipe Completion

--- Swipe/src/Deck.js
+++ Swipe/src/Deck.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { View, Animated, PanResponder, Dimensions } from "react-native";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
   constructor(props) {
@@ -17,10 +19,38 @@ class Deck extends Component {
         position.setValue({ x: gesture.dx, y: gesture.dy });
       },
       //after touch is released - let go
-      onPanResponderRelease: () => {}
+      onPanResponderRelease: (event, gesture) => {
+        if (gesture.dx > SWIPE_THRESHOLD) {
+          this.forceSwipe("right");
+        } else if (gesture.dx < -SWIPE_THRESHOLD) {
+          this.forceSwipe("left");
+        } else {
+          this.resetPosition();
+        }
+      }
     });
 
     this.state = { panResponder, position };
+  }
+
+  forceSwipe(direction) {
+    const x = direction === "right" ? SCREEN_WIDTH : -SCREEN_WIDTH;
+
+    Animated.timing(this.state.position, {
+      toValue: { x, y: 0 },
+      duration: SWIPE_OUT_DURATION
+    }).start(() => this.onSwipeComplete(direction));
+  }
+
+  onSwipeComplete(direction) {
+    const { onSwipeRight, onSwipeLeft } = this.props;
+    direction === "right" ? onSwipeRight() : onSwipeLeft();
+  }
+
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: { x: 0, y: 0 }
+    }).start();
   }
 
   getCardStyle() {
